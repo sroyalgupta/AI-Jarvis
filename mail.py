fc7a8e3baf1302773e93e9d68a3f93c4e15f9000
Sagar Gupta <Sroyalgupta@gmail.com>
2020-04-02 19:12:43 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -4,7 +4,7 @@ def sendmail(to,content):
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
     server.starttls()
-    server.login('sagarguptasargam123@gmail.com','srg@2909')
+    server.login('sagarguptasargam123@gmail.com','He he,you will not get it easily..')
     server.sendmail('sagarguptasargam123@gmail.com',to,content)
     server.close()
 
